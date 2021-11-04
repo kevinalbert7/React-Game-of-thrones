b4725d6f520a8db6c3d03f9d355ea865f2a78ac9
kevinalbert7 <20ko@live.fr>
2021-11-04 17:14:25 +0100

    GameOfThrone04

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Character from './components/Character'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
   constructor () {
--- src/components/Character.js
+++ src/components/Character.js
@@ -2,17 +2,22 @@ import React from 'react'
 
 class Character extends React.Component {
 	render() {
+        console.log("this.props.image:", this.props.image)
 		return(
             <>
-            <div>
-			{this.props.name}
-            </div>
-            <div>
-			{this.props.title}
-            </div>
-            <div>
-			{this.props.image}
-            </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                        <img src={this.props.image}/>
+                        </div>
+                        <div class="col">
+                        {this.props.title}
+                        </div>
+                        <div class="col">
+                        {this.props.name}
+                        </div>
+                    </div>
+                </div>
             </>
 		)
 	}
